af4a3989066b8c01e1e06ae644caf1c7c5f41e9f
Majd Ajroudi <majd.ajroudi@gmail.com>
2020-10-31 22:12:24 +0100

    commit tree 1b8aa41e51cfd8e730539b40ac7da84f0f5294ba

--- index.js
+++ index.js
@@ -3,17 +3,25 @@ const fi = (function() {
     libraryMethod: function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
+    each: function(arr, callbackFunc) {
+      Object.keys(arr).forEach((el) => {
+        callbackFunc(arr[el])
+        return arr
+      })
+      return arr
+    },
 
-    each: function() {
+    map: function(arr, callbackFunc) {
+      return Object.keys(arr).map((el,ind,col) => {
+        console.log(arr[el])
+        callbackFunc(arr[el], ind, arr)
+        return arr
+      })
 
     },
 
-    map: function() {
-
-    },
-
-    reduce: function() {
-
+    reduce: function(arr, callbackFunc,acc) {
+        arr.reduce((acc, el) => callbackFunc(acc,el,arr),0)
     },
 
     functions: function() {
